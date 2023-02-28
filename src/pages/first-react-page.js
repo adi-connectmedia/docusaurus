881c3b7dfdc77833318c2b3bb422168f05e9d2aa
adi-connectmedia <67589992+adi-connectmedia@users.noreply.github.com>
2023-02-28 12:51:03 +0200

    first commit

---
+++ src/pages/first-react-page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+    return (
+        <Layout>
+            <h1>Prima mea pagina React</h1>
+            <p>asta este o pagina React</p>
+        </Layout>
+    )
+}
