20f2d5d19deb6346251e5ea6ec18a1a0dcb13360
adi-connectmedia <67589992+adi-connectmedia@users.noreply.github.com>
2023-02-28 14:17:57 +0200

    add https in url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'connectmedia-docusaurus.netlify.app/',
+  url: 'https://connectmedia-docusaurus.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
