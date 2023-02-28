4a994fb6b0c028b5cf93e2273230708e09ec0da5
adi-connectmedia <67589992+adi-connectmedia@users.noreply.github.com>
2023-02-28 14:02:52 +0200

    change url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'connectmedia-docusaurus.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
